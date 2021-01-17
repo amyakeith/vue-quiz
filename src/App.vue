e0590675a2f9b71cfe3c531018b0cedd196f9f4e
amyakeith <amyakeith@gmail.com>
2021-01-16 19:32:32 -0600

    add navigation between questions

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset=3>
-          <QuestionBox />
+          <QuestionBox 
+            v-if="questions.length"
+            :currentQuestion = "questions[index]"
+            :next="next"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -21,12 +25,26 @@ export default {
     Header,
     QuestionBox
   },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    }
+  },
   mounted: function() {
     fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
       method: "get"
     })
       .then((response) => {
-        console.log(response.json())
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
       })
   }
 }
